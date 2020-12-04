d9e2c44c4d2948a551ff19eb8aeff3dedfebcf98
Owen Min <minyuwen@126.com>
2020-12-04 15:04:53 +0800

    base options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-  'extends': ['taro/react']
+  extends: ['plugin:taro/all'],
+  plugins: ['prettier', 'react-hooks'],
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'taro/render-props': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 }
--- src/pages/index/swiper.jsx
+++ src/pages/index/swiper.jsx
@@ -2,30 +2,32 @@ import React from 'react'
 import { Swiper, SwiperItem, Navigator, Image, View } from '@tarojs/components'
 
 function SwiperComponent(props) {
-
   const { data } = props
   console.log(props)
   return (
     <Swiper
-      className='test-h'
-      indicatorColor='#999'
-      indicatorActiveColor='#333'
+      className="test-h"
+      indicatorColor="#999"
+      indicatorActiveColor="#333"
       vertical={false}
       circular={false}
       indicatorDots
       autoplay
     >
-      {
-        data.map(item => {
-          return <SwiperItem key={item.id}>
-            <Navigator class='swiper-item' onClick={() => {console.log(123)}}>
-              <Image
-                src={item.url}
-              />
+      {data.map((item) => {
+        return (
+          <SwiperItem key={item.id}>
+            <Navigator
+              class="swiper-item"
+              onClick={() => {
+                console.log(123)
+              }}
+            >
+              <Image src={item.url} />
             </Navigator>
           </SwiperItem>
-        })
-      }
+        )
+      })}
     </Swiper>
   )
 }
