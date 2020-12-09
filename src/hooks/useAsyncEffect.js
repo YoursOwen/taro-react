ba9634aa7504b37c0a900a73f67a4331e621a554
Owen Min <minyuwen@126.com>
2020-12-09 17:22:24 +0800

    @fixed disable eslint

--- src/hooks/useAsyncEffect.js
+++ src/hooks/useAsyncEffect.js
@@ -3,5 +3,6 @@ import { useEffect } from 'react'
 export function useAsyncEffect(effect, deps) {
   useEffect(() => {
     effect()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps)
 }
